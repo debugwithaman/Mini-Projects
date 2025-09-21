8a7004c99f1a41e0aca9b8d1c3d74384b5f6ae17
debugwithaman <debugwithkazi@gmail.com>
2025-09-22 01:13:38 +0530

    Todo Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './Components/Todo'
+import TodoItems from './Components/TodoItems'
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+      
+    </div>
+  )
+}
+
+export default App
